977c00472a62526c2fe98956efdc40e8683dcaa8
Daniel Jost <torpedojost@gmail.com>
2014-10-10 19:45:55 -0400

    Tools framework connects to Dash via a button, React still incomplete.

---
+++ source/framework/component-object-browser.js
@@ -0,0 +1,79 @@
+/** @jsx React.DOM */
+
+/*global TreeView, React */
+
+// This example data format is totally arbitrary. No data massaging is
+// required and you use regular js in `render` to iterate through and
+// construct your nodes.
+dataSource = [
+  {
+    type: 'Employees',
+    collapsed: false,
+    people: [
+      {name: 'Paul Gordon', age: 25, sex: 'male', role: 'coder', collapsed: false},
+      {name: 'Sarah Lee', age: 23, sex: 'female', role: 'jqueryer', collapsed: false},
+    ]
+  },
+  {
+    type: 'CEO',
+    collapsed: false,
+    people: [
+      {name: 'Drew Anderson', age: 35, sex: 'male', role: 'boss', collapsed: false}
+    ]
+  }
+];
+
+// A controlled TreeView, akin to React's controlled inputs
+// (http://facebook.github.io/react/docs/forms.html#controlled-components), has
+// many benefits. Among others, you can expand/collapse everything (i.e. easily
+// trigger those somewhere else).
+var Lists = React.createClass({
+  getInitialState: function() {
+    var collapsedBookkeeping = this.props.dataSource.map(function() {
+      return false;
+    });
+    return {
+      collapsedBookkeeping: collapsedBookkeeping
+    };
+  },
+
+  handleClick: function(i) {
+    this.state.collapsedBookkeeping[i] = !this.state.collapsedBookkeeping[i];
+    this.setState({collapsedBookkeeping: this.state.collapsedBookkeeping});
+  },
+
+  collapseAll: function() {
+    this.setState({
+      collapsedBookkeeping: this.state.collapsedBookkeeping.map(function() {return true;})
+    });
+  },
+
+  render: function() {
+    var collapsedBookkeeping = this.state.collapsedBookkeeping;
+
+    return (
+      <div>
+        <button onClick={this.collapseAll}>Collapse all</button>
+
+        {this.props.dataSource.map(function(node, i) {
+          // Let's make it so that the tree also toggles when we click the
+          // label. Controlled components make this effortless.
+          var label =
+            <span className="node" onClick={this.handleClick.bind(null, i)}>
+              Type {i}
+            </span>;
+          return (
+            <TreeView key={i}
+              nodeLabel={label}
+              collapsed={collapsedBookkeeping[i]}
+              onClick={this.handleClick.bind(null, i)}>
+                {node.Components.map(function(entry) {
+                  return <div className="info">{entry}</div>;
+                })}
+            </TreeView>
+          );
+        }, this)}
+      </div>
+    );
+  }
+});
---
+++ source/framework/main.js
@@ -0,0 +1,74 @@
+/** @jsx React.DOM */
+var config = {
+  content: [{
+    type: 'row',
+    content: [{
+      type: 'stack',
+      width: 15,
+      content: [{
+        type: 'component',
+        componentName: 'object-browser',
+        title:'Object Browser'
+      }]
+    },
+    {
+      type: 'column',
+      width: 70,
+      content: [{
+        type: 'component',
+        componentName: 'testComponent',
+      },
+      {
+        type: 'component',
+        height: 20,
+        title: 'Console',
+        componentName: 'example-react'
+      }]
+    },
+    {
+      type: 'row',
+      content: [{
+        type: 'stack',
+        width: 15,
+        content: [{
+          type: 'component',
+          componentName: 'object-browser',
+          title:'Properties'
+        }]
+      }]
+    }]
+  }]
+};
+
+var myLayout = new GoldenLayout( config );
+
+var dash = new Dash();
+var dashObjectData = [];
+
+dash.onConnect = function() {
+  console.log('Connected to Dash.');
+  dash.getObjects( function(data) { dashObjectData = data; } )
+}
+
+myLayout.registerComponent( 'testComponent', function( container, state ){
+  container.getElement().html( '<button onClick="pressMeToo();">Connect to Dash</button>' );
+});
+
+myLayout.registerComponent( 'object-browser', function( container, state ){
+  React.renderComponent(
+    <Lists dataSource={dashObjectData} />,
+    container.getElement()[0]
+  );
+});
+
+pressMeToo = function() {
+  dash.connect('8008');
+  console.log('Connecting to Dash...');
+}
+
+myLayout.registerComponent( 'example-react', function( container, state ){
+  container.getElement()[0].style.overflow = 'auto';
+  //setInterval(function() { container.getElement().html( container.getElement().html() + '<span>This is a console window.</span><br />' ) }, 5000);
+});
+
+myLayout.init();
