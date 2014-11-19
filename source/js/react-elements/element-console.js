a5a25f694e0995a2ac6022065c83a2c9fcf8be1b
Brandon Littell <brandon@brandonlittell.com>
2014-11-19 16:00:17 -0500

    [Console] Fixed element keys

--- source/js/react-elements/element-console.js
+++ source/js/react-elements/element-console.js
@@ -1,7 +1,7 @@
 var DashConsoleList = React.createClass({
   render: function() {
     var logItem = function(item) {
-      return <li key={ item.timestamp }>{item.msg}</li>;
+      return <li key={ item.id }>{item.msg}</li>;
     };
     return (
       <ul>{this.props.items.map(logItem)}</ul>
@@ -10,16 +10,20 @@ var DashConsoleList = React.createClass({
 });
 var DashConsole = React.createClass({
   getInitialState: function() {
-    return {items: []};
+    return {items: [], keyCount: 0};
   },
   log: function(item) {
     var nextItems;
+    var key = this.state.keyCount.value;
     if(typeof item === "string")
-      nextItems = this.state.items.concat([{msg: item}]);
+      nextItems = this.state.items.concat([{msg: item, id: key}]);
     else
+    {
+      item.id = key;
       nextItems = this.state.items.concat([item]);
+    }
 
-    this.setState({items: nextItems});
+    this.setState({items: nextItems, keyCount: key++});
   },
   render: function() {
     return (
