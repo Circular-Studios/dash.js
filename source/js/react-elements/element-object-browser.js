bc3535c5da9cf29cd29a37fb79751e57e97960bc
Daniel Jost <daniel@danieljost.com>
2014-11-19 17:55:37 -0500

    Added object deletion on the tools side.

--- source/js/react-elements/element-object-browser.js
+++ source/js/react-elements/element-object-browser.js
@@ -5,15 +5,23 @@ var DashObjects = React.createClass({
     onClick: function( i ) {
       dashSelectedProperties.setProps( { data: dashObjectData[ i ].Components } );
     },
+    removeObject: function( i ) {
+      dashObjectData.splice( i, i + 1 );
+      dashObjectList.setProps( { data: dashObjectData } );
+    },
     render: function() {
     return (
       <div>
         { this.props.data.map( function( node, i ) {
           var self = this;
+          var close = new React.DOM.span({
+            className: 'close-button',
+            onClick: function() { self.removeObject( i ); }
+          }, '(x)' );
           var label = new React.DOM.span({
             className: 'node',
-            onClick: function() { self.onClick( i ) }
-          }, node.Name )
+            onClick: function() { self.onClick( i );}
+          }, node.Name, close );
           return (
             <TreeView key={ node.Name + '|' + i } nodeLabel={ label } defaultCollapsed={ true }>
               { node.Children.map( function( child, j ) {
